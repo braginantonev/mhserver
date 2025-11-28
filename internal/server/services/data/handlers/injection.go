0f50b22b43a9e8b3f2187d572b9af4c7e45cbb44
Bragin Anton <antonsayfermay@gmail.com>
2025-11-28 18:44:05 +0300

    [data_handlers] add injections

---
+++ internal/server/services/data/handlers/injection.go
@@ -0,0 +1,28 @@
+package data_handlers
+
+import (
+	"net/http"
+
+	"github.com/braginantonev/mhserver/pkg/data"
+	pb "github.com/braginantonev/mhserver/proto/data"
+)
+
+type DataHandler interface {
+	SaveData(w http.ResponseWriter, r *http.Request)
+}
+
+type Config struct {
+	DataConfig        data.Config
+	MaxRequestsCount  int
+	DataServiceClient pb.DataServiceClient
+}
+
+type Handler struct {
+	cfg Config
+}
+
+func NewDataHandler(cfg Config) Handler {
+	return Handler{
+		cfg: cfg,
+	}
+}
