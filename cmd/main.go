d9be24ec43a8451e4728144b7a698444ec5038be
Bragin Anton <antonsayfermay@gmail.com>
2025-12-06 17:53:14 +0300

    [main] add selection app mode

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,25 @@ import (
 	"github.com/braginantonev/mhserver/internal/application"
 )
 
+var (
+	ArgAppMode = map[string]application.ApplicationMode{
+		"-M": application.AppMode_MainServerOnly,
+		"-S": application.AppMode_SubServersOnly,
+	}
+)
+
 func main() {
 	app := application.NewApplication()
-	if err := app.Run(application.AppMode_AllServers); err != nil {
+
+	app_mode := application.AppMode_AllServers
+	for _, arg := range os.Args {
+		mode, ok := ArgAppMode[arg]
+		if ok {
+			app_mode = mode
+		}
+	}
+
+	if err := app.Run(app_mode); err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
