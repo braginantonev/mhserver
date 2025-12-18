e4f09a879893a83aea16580e775c60b771bd2f21
Bragin Anton <antonsayfermay@gmail.com>
2025-12-18 15:26:45 +0300

    [sql] add base scripts

---
+++ sql/create-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS mhserver;
+GRANT ALL PRIVILEGES ON mhserver.* TO 'mhserver'@'localhost';
+
+CREATE DATABASE IF NOT EXISTS mhserver_test;
+GRANT ALL PRIVILEGES ON mhserver_test.* TO 'mhserver'@'localhost';
