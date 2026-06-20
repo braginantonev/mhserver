a3a4954ad86ca74c2bfd9add689dcc8a02c688c8
Bragin Anton <antonsayfermay@gmail.com>
2026-06-20 13:56:22 +0300

    [repository] add ratelimit

---
+++ internal/repository/ratelimit/ratelimit.go
@@ -0,0 +1,182 @@
+package ratelimit
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+const (
+	USER_RATE_LIFETIME     = time.Minute
+	LIMITER_CLEAN_DURATION = USER_RATE_LIFETIME // temp
+)
+
+type AcceptedRequests struct {
+	delete     chan any
+	count      int
+	next_reset int64
+}
+
+func NewAcceptedRequests(reset_duration time.Duration) *AcceptedRequests {
+	reqs := &AcceptedRequests{
+		delete: make(chan any),
+	}
+	go reqs.startResetTicker(reset_duration)
+	return reqs
+}
+
+func (r *AcceptedRequests) reset() {
+	r.count = 0
+}
+
+func (r *AcceptedRequests) startResetTicker(reset_duration time.Duration) {
+	ticker := time.NewTicker(reset_duration)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-r.delete:
+			return
+		case <-ticker.C:
+			r.reset()
+			r.next_reset = time.Now().Add(reset_duration).Unix()
+		}
+	}
+}
+
+func (r *AcceptedRequests) NextReset() int64 {
+	return r.next_reset
+}
+
+func (r *AcceptedRequests) Count() int {
+	return r.count
+}
+
+func (r *AcceptedRequests) Add() {
+	r.count += 1
+}
+
+func (r *AcceptedRequests) Delete() {
+	close(r.delete)
+}
+
+type UserRate struct {
+	reqs *AcceptedRequests
+	exp  int64
+}
+
+func NewUserRate(limiter_interval time.Duration) *UserRate {
+	return &UserRate{
+		reqs: NewAcceptedRequests(limiter_interval),
+		exp:  time.Now().Add(USER_RATE_LIFETIME).Unix(),
+	}
+}
+
+func (u *UserRate) UpdateExpiration() {
+	u.exp = time.Now().Add(USER_RATE_LIFETIME).Unix()
+}
+
+func (u *UserRate) ResetRequestsAfter() int64 {
+	return u.reqs.NextReset() - time.Now().Unix()
+}
+
+func (u *UserRate) AcceptedRequests() int {
+	return u.reqs.Count()
+}
+
+func (u *UserRate) RegisterRequest() {
+	u.reqs.Add()
+	u.UpdateExpiration()
+}
+
+type Limiter struct {
+	mux      *sync.RWMutex
+	limit    int
+	interval time.Duration
+	occupied map[string]*UserRate
+}
+
+func NewLimiter(ctx context.Context, max_requests int, interval time.Duration) *Limiter {
+	limiter := &Limiter{
+		mux:      &sync.RWMutex{},
+		limit:    max_requests,
+		interval: interval,
+		occupied: make(map[string]*UserRate),
+	}
+	go limiter.startCleaner(ctx)
+	return limiter
+}
+
+func (l *Limiter) cleanExpired() {
+	now := time.Now().Unix()
+
+	l.mux.Lock()
+	for ip, rate := range l.occupied {
+		if rate.exp <= now {
+			delete(l.occupied, ip)
+		}
+	}
+	l.mux.Unlock()
+}
+
+func (l *Limiter) startCleaner(ctx context.Context) {
+	ticker := time.NewTicker(LIMITER_CLEAN_DURATION)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			l.cleanExpired()
+		}
+	}
+}
+
+// Возвращает true, если пользователь не достиг лимита запросов.
+func (l *Limiter) Allow(ip string) bool {
+	l.mux.RLock()
+	defer l.mux.RUnlock()
+	if rate, ok := l.occupied[ip]; ok {
+		return l.limit-rate.AcceptedRequests() != 0
+	}
+	return true // Если пользователь отсутствует, то это его первый запрос
+}
+
+func (l *Limiter) AllowAfter(ip string) int64 {
+	l.mux.RLock()
+	defer l.mux.RUnlock()
+
+	rate, ok := l.occupied[ip]
+	if !ok {
+		return 0
+	}
+
+	if l.limit-rate.AcceptedRequests() != 0 {
+		return 0
+	}
+
+	// Сброс запросов в интервале всегда будет больше текущего времени
+	return rate.ResetRequestsAfter() - time.Now().Unix()
+}
+
+func (l *Limiter) Occupy(ip string) {
+	l.mux.Lock()
+	if rate, ok := l.occupied[ip]; ok {
+		rate.RegisterRequest()
+	} else {
+		l.occupied[ip] = NewUserRate(l.interval)
+	}
+	l.mux.Unlock()
+}
+
+func (l *Limiter) Remaining(ip string) int {
+	l.mux.RLock()
+	defer l.mux.RUnlock()
+
+	rate, ok := l.occupied[ip]
+	if !ok {
+		return l.limit
+	}
+	return l.limit - rate.AcceptedRequests()
+}
