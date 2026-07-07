7230b1481b2eceb3033fbef1c0834ec5b8bd8b26
Bragin Anton <antonsayfermay@gmail.com>
2026-07-07 15:54:41 +0300

    [appconfig] add extra params

--- internal/config/application/application.go
+++ internal/config/application/application.go
@@ -13,14 +13,20 @@ type SubServer struct {
 	Enabled bool
 	Address string
 	Port    int
+	Extra   SubServerExtra
+}
+
+type SubServerExtra struct {
+	Priority        int
+	AllocatedMemory uint64 `toml:"allocated_memory"`
 }
 
 type ApplicationConfig struct {
-	WorkspacePath string              `toml:"workspace_path"`
-	JWTSignature  string              `toml:"jwt_signature"`
-	DB_Pass       string              `toml:"db_pass"`
-	Memory        config.MemoryConfig `toml:"memory"`
-	SubServers    map[string]SubServer
+	WorkspacePath string `toml:"workspace_path"`
+	JWTSignature  string `toml:"jwt_signature"`
+	DB_Pass       string `toml:"db_pass"`
+	Memory        config.MemoryConfig
+	SubServers    map[string]*SubServer
 }
 
 func NewApplicationConfig(config_path, db_name string) ApplicationConfig {
@@ -31,10 +37,27 @@ func NewApplicationConfig(config_path, db_name string) ApplicationConfig {
 	}
 
 	slog.Info("Configuration loaded.")
-	slog.Info(fmt.Sprintf("Available server ram: %d bytes", cfg.Memory.AvailableRAM))
+	slog.Info(fmt.Sprintf("Server allocated memory: %d bytes", cfg.Memory.Allocated))
 	slog.Info(fmt.Sprintf("Server will be started at %s:%d", cfg.SubServers["main"].Address, cfg.SubServers["main"].Port))
 	slog.Info(fmt.Sprintf("Server configured to use \"mhserver/%s\" database", db_name))
 	slog.Info(fmt.Sprintf("Server workspace path = %s", cfg.WorkspacePath))
+
+	// allocate memory for subserver's
+	var priority_sum int
+	for _, srv := range cfg.SubServers {
+		if srv.Enabled && srv.Extra.AllocatedMemory == 0 {
+			priority_sum += srv.Extra.Priority
+		}
+	}
+
+	mem_chunk := cfg.Memory.Allocated / uint64(priority_sum)
+	for _, srv := range cfg.SubServers {
+		if srv.Enabled && srv.Extra.AllocatedMemory == 0 {
+			srv.Extra.AllocatedMemory = mem_chunk * uint64(srv.Extra.Priority)
+		}
+	}
+
+	// ^ mb that's look like a shit
 
 	return cfg
 }
