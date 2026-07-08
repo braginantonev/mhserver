f1a535d75725b3a1cd89b8b0c567b691fd82ff6c
Bragin Anton <antonsayfermay@gmail.com>
2026-07-08 16:53:53 +0300

    [build] update version

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Run in root of project tree
 
+export VERSION=${VERSION:-$(git describe --tags --dirty --always | sed -e "s/^v//g")}
+
 if [[ !(-e build) ]]; then
     mkdir build
 else
     rm -rf build/*
 fi
 
-go build -C cmd/ -o mhserver
+go build -C cmd/ -o ../build/mhserver -ldflags="-s -w -X=github.com/braginantonev/version.Version=${VERSION}"
 mv cmd/mhserver build/
 
 cp -r scripts/ build/scripts/
